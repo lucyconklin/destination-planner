e26992e69f8293ec2358f02d98b9ed1746698b7e
Lucy Conklin <lucy.conklin@gmail.com>
2017-02-21 21:57:48 -0700

    Begin API test

---
+++ spec/requests/api/v1/destinations/crud_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Weather API' do
+  before do
+    
+  end
+  xit 'can return one destination' do
+    get "/api/v1/destinations/#{@destination.id}"
+  end
+
+  xit 'can create a desitionation' do
+
+  end
+
+  xit 'can edit a destination' do
+
+  end
+
+  xit 'can delete a destination' do
+
+  end
+end
